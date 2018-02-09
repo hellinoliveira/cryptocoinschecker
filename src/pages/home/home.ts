072776aecefeeb8f6df0ae3019692c439fd36161
hellison.oliveira <hellison.oliveira@gmail.com>
2018-02-09 15:07:29 -0200

    trying some coins api

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,15 +10,53 @@ import { RestProvider } from '../../providers/rest/rest';
 })
 export class HomePage {
 
+  items = [];
+  data: any;
+
   @Output()
   currentCoin: CryptoCoin = new CryptoCoin('BTC', 'Bitcoin');
   coins: CryptoCoin[] = [];
   users: any;
 
   constructor(public navCtrl: NavController, public restProvider: RestProvider) {
-    this.updatePage();
+    // for (let i = 0; i < 30; i++) {
+    //   this.items.push(this.items.length);
+    // }
+    // this.updatePage();
+    // this.getAllCoins();
     // this.getUsers();
+    this.doInfinite();
   }
+
+  // doInfinite(infiniteScroll) {
+  //   console.log('Begin async operation');
+
+  //   setTimeout(() => {
+  //     for (let i = 0; i < 30; i++) {
+  //       this.items.push(this.items.length);
+  //     }
+
+  //     console.log('Async operation has ended');
+  //     infiniteScroll.complete();
+  //   }, 500);
+  // }
+
+  doInfinite(): Promise<any> {
+    this.restProvider.getAllCoinsData()
+      .then(data => {
+        this.data = data;
+      });
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        for (let i = 0; i < this.data.Data.length; i++) {
+          this.getSingleCoinPrice(this.data.Data[i].CoinInfo.Name);
+          this.items.push(this.data.Data[i].CoinInfo);
+        }
+        resolve();
+      }, 500);
+    })
+  }
+
   // getUsers() {
   //   this.restProvider.getUsers()
   //     .then(data => {
@@ -32,8 +70,21 @@ export class HomePage {
         this.currentCoin.brlValue = data['BRL'];
         this.currentCoin.usdValue = data['USD'];
         this.currentCoin.euroValue = data['EUR'];
-        console.log(this.currentCoin);
       });
+  }
+
+  getAllCoins() {
+    this.restProvider.getAllCoinsData()
+      .then(data => {
+      });
+  }
+
+  getSingleCoinPrice(coin)
+  {
+    this.restProvider.getSinglePrice(coin)
+    .then(data => {
+      console.log(data);
+    }); 
   }
 
 }
